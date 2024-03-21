d915e3ad81d4bd2c1b7b44ed65d71d3ffd1e0c6e
Gedebie Endalew <gedeabera89@gmail.com>
2024-03-21 05:25:04 +0300

    commit

--- pdfprocessor/admin.py
+++ pdfprocessor/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import DocumentAnalysis, PDFfile
+admin.site.register(DocumentAnalysis)
+admin.site.register(PDFfile)
+
