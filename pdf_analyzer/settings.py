b3e3ef1f3375930dccc45f705acf6c0e94633374
Gedebie Endalew <gedeabera89@gmail.com>
2024-03-27 11:49:13 +0300

    updated

--- pdf_analyzer/settings.py
+++ pdf_analyzer/settings.py
@@ -9,23 +9,32 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import os
+#import dj_database_url
+
 
 from pathlib import Path
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR/".eVar", ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*vu4=%)(#a5+7hkm@@=cri5vu&ysvyo^%on)y_h--d0pgv1q5z"
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split()
 
 
 # Application definition
@@ -84,7 +93,7 @@ WSGI_APPLICATION = "pdf_analyzer.wsgi.application"
 # }
 
 DATABASES = {
-    "default": {
+    'default' : {
         "ENGINE": "djongo",
         "NAME": "pdfanalyzer",
         "ENFORCE_SCHEMA": False,
@@ -93,6 +102,7 @@ DATABASES = {
         
         },
     }
+    
 }
 
 
