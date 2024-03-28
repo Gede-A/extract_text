54c247c51c06787e2e336a0d69ef3f6a05565b45
Gedebie Endalew <gedeabera89@gmail.com>
2024-03-28 12:44:44 +0300

    updated

--- pdf_analyzer/settings.py
+++ pdf_analyzer/settings.py
@@ -85,12 +85,10 @@ WSGI_APPLICATION = "pdf_analyzer.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-'''DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-     }
-}'''
+# "default": {
+        # "NAME": BASE_DIR / "db.sqlite3",
+    # }
+#}#'''
 
 DATABASES = {
     "default" : {
